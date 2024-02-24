2b411ff04d7dcb55153adf58747c1da0a4bc6d9b
quentinshin34 <tvy6kv@virginia.edu>
2024-02-23 20:47:20 -0500

    Add file madlib.sh for lab 3

---
+++ mywork/labs/madlib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+clear
+echo "Let's build a mad-lib!"
+
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me a noun: " NOUN1
+read -p "1. Please give me a name: " NAME1
+read -p "1. Please give me an adverb: " ADV1
+read -p "1. Please give me a verb: " VERB1
+read -p "1. Please give me a verb: " VERB2
+read -p "1. Please give me a noun: " NOUN2
+read -p "1. Please give me an adjective: " ADJ2
+read -p "1. Please give me an adjective: " ADJ3
+
+echo "There was once a $ADJ1 $NOUN1 named $NAME1. $NAME1 would often $ADV1 $VERB1 and was famous for it.
+One day $NAME1 decided to $VERB2 with another $NOUN1. All of sudden a $NOUN2 joined them and
+even though they were not invited to $VERB2. $NAME1 was pleasntly surprised and thought that $NOUN2 was very $ADJ2 and $ADJ3
+and enjoyed hanging out."
